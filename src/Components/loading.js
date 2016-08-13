bcc18a83d207ac7338058108810391f2ec549861
Yeiber Cano <yeibercano@gmail.com>
2016-08-13 02:45:23 -0700

    clean up extra spacing

--- src/Components/loading.js
+++ src/Components/loading.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
 import Loading from 'react-loading';
 
-
-
 class Load extends Component{
 
   render() {
     return (
-        <Loading type='spinningBubbles' color='white' height='800' width='800' />
-      )
+      <Loading type='spinningBubbles' color='white' height='800' width='800' />
+    );
   }
 } 
 
