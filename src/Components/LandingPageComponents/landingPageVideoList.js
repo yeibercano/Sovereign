05c4e0d093b5d0261ba6350cbb1e85948705821f
Yeiber Cano <yeibercano@gmail.com>
2016-08-13 02:45:50 -0700

    add key to children

--- src/Components/LandingPageComponents/landingPageVideoList.js
+++ src/Components/LandingPageComponents/landingPageVideoList.js
@@ -23,7 +23,7 @@ class LandingPageVideoList extends Component {
    if (movieInfo.rating > 2) {
        if (movieInfo.rating < 15) {
          return (
-       <section onClick={(movieSelected) => this.movieSelected(movieInfo)} className="landing_page_movieInfo">
+       <section key={movieInfo.image} onClick={(movieSelected) => this.movieSelected(movieInfo)} className="landing_page_movieInfo">
          <a href={(movieSelected) => this.movieSelected(movieInfo)}>
            <img id="landing_page_movieInfo_image"src={movieInfo.image} /> 
          </a>
