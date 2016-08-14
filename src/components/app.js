b5cc4b9f391d2ce484066b41e3322360557ed868
Yeiber Cano <yeibercano@gmail.com>
2016-08-14 02:58:44 -0700

    create add component

---
+++ src/components/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Footer from './footer'
+import Header from './Header/Header'
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+      <Header />
+        <section className="wrapper">
+          {this.props.children}
+        </section>
+      <Footer />
+    </section>
+    );
+  }
+}
+
+export default App;
