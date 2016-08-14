cbdf812f83da3ced7ac251c84ba36ea0029a98de
Yeiber Cano <yeibercano@gmail.com>
2016-08-14 04:00:01 -0700

    create getMovies action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
 import axios from 'axios';
+import * as types from './types'
+
+export function getMovies() {
+	return dispatch => {
+    axios.get('/movies')
+    .then( response => { 
+    	dispatch({ type: types.RECEIVE_MOVIES, payload: response.data })
+    });
+  }
+} 
