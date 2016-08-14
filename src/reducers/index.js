194c8225307781a9c64aa2e5a4c52556c0730cbb
Yeiber Cano <yeibercano@gmail.com>
2016-08-14 04:01:17 -0700

    create RECEIVE_MOVIES reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
+import { RECEIVE_MOVIES } from '../actions/types'
 
-
-export default function(state ={}, action) {
-	
+export default function(state ={list:[]}, action) {
+	switch (action.type) {
+		case RECEIVE_MOVIES:
+			return { ...state, list: action.payload }
+	}
 	return state;
 };
