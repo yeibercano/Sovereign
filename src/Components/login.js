70a134fbbeb390bbd88ebef25e770cf64a05c49b
Yeiber Cano <yeibercano@gmail.com>
2016-08-15 22:50:16 -0700

    remove unnecessary modules and comments

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-var axios = require('axios');
-import { Router, Redirect, Route, IndexRoute, Link, hashHistory, browserHistory} from 'react-router'
-
-
+import axios from 'axios'
+import { hashHistory } from 'react-router'
 
 // var  local = JSON.parse(localStorage.getItem('u'))
 // let token = local.token;
@@ -11,27 +9,10 @@ import { Router, Redirect, Route, IndexRoute, Link, hashHistory, browserHistory}
 class Login extends React.Component {
 
   constructor (props) {
-    //Signifies what is coming from the super class?
-    // console.log("For kicks: ", this.props.userInfo);
-    // get request to db to retrieve the movie nodes and subsequent video url contained within
-    // let urlServer = '';
-    // axios.get("/movies", {params: {userName: this.props.userInfo.userName}})
-    // .then(function(movieResponse){
-    //   urlServer = movieResponse.data[movieResponse.data.length-1].m.properties.video
-      
-    //   // TODO: Get video url from movieResponse at very last index if it comes back in an array
-    //   // remember to put the component back into profile once finished with TODO. Place array of movies into allMovies property
-    // })
     super (props)  
       let userLS = localStorage.getItem('user');
       //parses the info brought down (object)
       let parseUser = JSON.parse(userLS);
-      // console.log('userLS PROFILE COMPONENT', parseUser);
-      // this.props.user = localStorage.getItem(JSON.parse(user))
-      // let movieLS = localStorage.getItem('movieInfo');
-      //parses the info brought down (object)
-      // let parseMovie = JSON.parse(movieLS);
-      // console.log('this is the new parsed movie info:', parseMovie)
     this._userLogin = this._userLogin.bind(this);
     this.state = {
      user: parseUser,
@@ -39,8 +20,6 @@ class Login extends React.Component {
     }
    }
     
-
-
   _userLogin(e){
     e.preventDefault();
     let userLogin = {
@@ -58,12 +37,11 @@ class Login extends React.Component {
       hashHistory.push('home')
     }).catch(function(err){
       if (err ) throw err
-    })
+    });
   }
 
-
-    render () {
-      return(
+  render () {
+    return(
       <div>
         <form onSubmit={this._userLogin} >
           <input
@@ -83,7 +61,7 @@ class Login extends React.Component {
             className="login-button" />
         </form>
       </div>
-    )
+    );
   }
 }
 
