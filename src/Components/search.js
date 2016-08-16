01d0d88d672c3ab76497ebbbf6d6fb43cc68c664
Yeiber Cano <yeibercano@gmail.com>
2016-08-15 22:09:00 -0700

    code clean up

--- src/Components/search.js
+++ src/Components/search.js
@@ -56,19 +56,3 @@ class Search extends Component{
 
 export default Search
 
-    // if (!this.props.sResults) {
-    //   return <div>Loading...</div>
-    // }
-
-    //       <section className="search-item-container" ></section>
-    //       <img src={result.image} />
-    //       <h3>{result.title}</h3>
-
-    // {this.props.sResults.map(result=> this.displayResult(result))}
-// onClick={()=> this.props.selectedMovie(videoInfo)}
-
-          // <section className="search-item-container" onClick={()=> this.props.selectedMovie(videoInfo)}></section>
-          // <img src={videoInfo.image} />
-          // <h3>{videoInfo.title}</h3>
-
-          //      {this.state.searchResults.map(result=> this.displayResult(result))}
