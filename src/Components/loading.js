aa2ccf349b35376bd8007d7959e2cc14541c55eb
Yeiber Cano <yeibercano@gmail.com>
2016-08-19 23:50:39 -0700

    refactor to functional component

--- src/Components/loading.js
+++ src/Components/loading.js
@@ -1,13 +1,10 @@
 import React, {Component} from 'react';
 import Loading from 'react-loading';
 
-class Load extends Component{
-
-  render() {
-    return (
-      <Loading type='spinningBubbles' color='white' height='800' width='800' />
-    );
-  }
+const Load = () => {
+  return (
+    <Loading type='spinningBubbles' color='white' height='800' width='800' />
+  );
 } 
 
 export default Load
