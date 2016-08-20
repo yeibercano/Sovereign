49656ec2861126f5bb7b2bbbe0eb2d538c46b715
Yeiber Cano <yeibercano@gmail.com>
2016-08-19 21:55:54 -0700

    remove extra file calls and cleans code

--- src/Components/VotingComponent/votingPlayer.js
+++ src/Components/VotingComponent/votingPlayer.js
@@ -1,39 +1,21 @@
-import React, {Component} from 'react'
-
-var secret = require("../../../private.js")
-var axios = require('axios');
+import React, { Component } from 'react'
 
 class VoteVideoPlayer extends React.Component {
   constructor (props) {
     super (props) 
-    
-    // let textvid = document.getElementsByTagName('video'); 
-    // console.log("This is textvid", textvid);
   }
 
-   ratingTimeout(){
-    console.log("made it into ratingTimeout");
+  ratingTimeout(){
     alert("Voting for the movie" + this.props.title + "is about to close, please rate");
   }
 
-   movieEnded (e, movie){
+  movieEnded (e, movie){
     e.preventDefault();
     alert("You may now vote on this movie");
     let what = document.getElementById("star-rating")
     what.style.visibility= "visible"
-    // console.log("This is movie: ",movie);
-
-    //Alert user that voting will be closing for a particular movie
-    // window.setTimeout(function(){
-    //   console.log("made it into ratingTimeout");
-    //   alert("Voting for the movie " + movie + " is about to close, please rate");
-    // }.bind(this), 25000)
-
     return 
-    //update tha plays on the node for this specific movie
-    // axios.post()
-   }
-
+  }
 
   render() {
     return (
