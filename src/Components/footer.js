315203fa52654bf5a45ff42630b42b99f32fe982
Yeiber Cano <yeibercano@gmail.com>
2016-08-19 23:47:30 -0700

    refactor to functional component

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 
-class Footer extends Component {
- 
-  render() {
-  	const date = new Date().getFullYear();
-    return (
-      <footer className="footerBackground"> 
-        <h3>Sovereign &copy; { date }</h3>
-      </footer>
-    );
-  }
+const Footer = () => {
+	const date = new Date().getFullYear();
+
+  return (
+    <footer className="footerBackground"> 
+      <h3>Sovereign &copy; { date }</h3>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
