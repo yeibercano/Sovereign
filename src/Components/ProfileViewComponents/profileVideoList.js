628232abf67451299168adb0cacd03451a23eb7a
Yeiber Cano <yeibercano@gmail.com>
2016-08-19 23:58:41 -0700

    remove extra imports and fix indentation

--- src/Components/ProfileViewComponents/profileVideoList.js
+++ src/Components/ProfileViewComponents/profileVideoList.js
@@ -1,18 +1,16 @@
 import React, {Component} from 'react'
 import {Carousel, CarouselItem, CarouselCaption} from 'react-bootstrap'
-var secret = require("../../../private.js")
-var axios = require('axios');
 
 class ProfileVideoList extends Component {
  
   videoInfo(videoInfo) {
     return (
-        <Carousel.Item className="list-item-container" >
-          <img id="mini_carousel" src={videoInfo.image} onClick={()=> this.props.selectedMovie(videoInfo)}/>
-          <Carousel.Caption>
-            <h3>{videoInfo.title}</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <Carousel.Item className="list-item-container" >
+        <img id="mini_carousel" src={videoInfo.image} onClick={()=> this.props.selectedMovie(videoInfo)}/>
+        <Carousel.Caption>
+          <h3>{videoInfo.title}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
     )
   }
   render() {
@@ -23,7 +21,7 @@ class ProfileVideoList extends Component {
       <section>
         <h1 className="upload_list_title">Your Uploaded Content</h1>
         <Carousel className="profile_video_list">
-            {this.props.moviesList.map(movie => this.videoInfo(movie.m.properties))}
+          {this.props.moviesList.map(movie => this.videoInfo(movie.m.properties))}
         </Carousel>
       </section>
     );
