32d73122b1f7d3b339ab5ef961c55ce46059054d
Yeiber Cano <yeibercano@gmail.com>
2016-08-20 03:07:05 -0700

    combine last reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import { category } from './receive_category_reducer'
 import { movies } from './receive_movies_reducer'
+import { last } from './user_last_movie_reducer'
 
 
  const reducers = combineReducers({
  	category,
  	movies,
+ 	last
  })
 
 export default reducers;
