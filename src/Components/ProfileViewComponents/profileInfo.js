1b0a1c4c1b5293739b38bca93c1a6e1d464e13b2
Yeiber Cano <yeibercano@gmail.com>
2016-08-23 01:08:30 -0700

    code clean up

--- src/Components/ProfileViewComponents/profileInfo.js
+++ src/Components/ProfileViewComponents/profileInfo.js
@@ -12,7 +12,11 @@ class ProfileInfo extends Component {
       <aside  className="account_information">
         <h1 id="account_information_title" >Welcome, {this.props.userInfo.userName}!</h1>
         <section id="account_information_gravatar_container">
-        <Gravatar id="account_information_gravatar" email={this.props.userInfo.email.toLowerCase().trim()} default="monsterid" size={200} rating="pg"></Gravatar>
+        <Gravatar 
+          id="account_information_gravatar" 
+          email={this.props.userInfo.email.toLowerCase().trim()} 
+          default="monsterid" size={200} rating="pg">
+        </Gravatar>
         </section>
         <h4 id="full_name">{this.props.userInfo.firstName} {this.props.userInfo.lastName}</h4>
         <h4>{this.props.userInfo.email}</h4>
