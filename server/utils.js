bdb4022a234fb783c61c78708c488786c51f6f07
Yeiber Cano <yeibercano@gmail.com>
2016-08-26 22:03:30 -0700

    fix indentation

--- server/utils.js
+++ server/utils.js
@@ -1,13 +1,14 @@
 function authenticate (req, res, next){
     var body = req.body;
     if(!body.userName || !body.password) {
-        res.status(400).end('Must provide username and password')
+      res.status(400).end('Must provide username and password')
     }
 
     if((body.userName !== user.userName || body.password !== user.password)) {
-        res.status(401).end('Username or password incorrect');
+      res.status(401).end('Username or password incorrect');
     }
     next();
 }
 
 module.exports = authenticate;
+
