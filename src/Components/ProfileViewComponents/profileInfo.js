ecf61b194f0fdd79109f68762f570b4d68305d2c
Yeiber Cano <yeibercano@gmail.com>
2016-08-28 03:32:40 -0700

    remove constructor

--- src/Components/ProfileViewComponents/profileInfo.js
+++ src/Components/ProfileViewComponents/profileInfo.js
@@ -2,10 +2,6 @@ import React, {Component} from 'react'
 import Gravatar from 'react-gravatar'
  
 class ProfileInfo extends Component {
-  
-  constructor(props){
-    super(props)
-  }
 
   render() {
     return (
