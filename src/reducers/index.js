fd2f29f8bd9e258cd0897ad7dbcb1475034f40d7
Yeiber Cano <yeibercano@gmail.com>
2016-08-27 17:47:14 -0700

    add signin reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,13 @@ import { combineReducers } from 'redux'
 import { category } from './receive_category_reducer'
 import { movies } from './receive_movies_reducer'
 import { last } from './user_last_movie_reducer'
-
+import { signin } from './signin_reducer'
 
  const reducers = combineReducers({
  	category,
  	movies,
- 	last
+ 	last,
+ 	signin
  })
 
 export default reducers;
