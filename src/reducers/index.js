aa1bbf5e40d43918a2f57525546668a9625eecf7
Yeiber Cano <yeibercano@gmail.com>
2016-08-28 01:13:32 -0700

    import search reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { category } from './receive_category_reducer'
 import { movies } from './receive_movies_reducer'
 import { last } from './user_last_movie_reducer'
 import { signin } from './signin_reducer'
+import { search } from './search_reducer'
 
  const reducers = combineReducers({
  	category,
  	movies,
  	last,
- 	signin
+ 	signin, 
+ 	search
  })
 
 export default reducers;
