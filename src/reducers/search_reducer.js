fa255c61c7a651a029e7243afedd82882547b5ba
Yeiber Cano <yeibercano@gmail.com>
2016-08-28 01:14:08 -0700

    create search reducer

--- src/reducers/search_reducer.js
+++ src/reducers/search_reducer.js
@@ -1,12 +1,11 @@
 import { SEARCH } from '../actions/types'
 
-export function(state = { results:[]}, action) {
+export function search(state = { results:[]}, action) {
 	switch (action.type) {
 		case SEARCH:
 			return {
 				...state, results: action.payload
 			}
-		default:
-		  return state
 	}
+	return state
 }
