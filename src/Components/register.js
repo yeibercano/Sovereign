386a5f0940df39f568a053292a77597799e3649e
Yeiber Cano <yeibercano@gmail.com>
2016-08-30 20:35:20 -0700

    refactor to use Redux

--- src/Components/register.js
+++ src/Components/register.js
@@ -1,21 +1,14 @@
-var React = require('react');
-var axios = require('axios');
-var secret = require("../../private.js")
-import { Router, Redirect, Route, IndexRoute, Link, hashHistory, browserHistory} from 'react-router'
+import React, { Component } from 'react'
+import secret from '../../private.js'
+// import { Router, Redirect, Route, IndexRoute, Link, hashHistory, browserHistory} from 'react-router'
+import { connect } from 'react-redux'
 
 
+class CreateAccountScreen extends Component {
 
-var CreateAccountScreen = React.createClass({
-
-  _handleChange: function(e) {
-    // this.setState({value: e.target.value});
-  },
-
-    _saveAndContinue(e) {
-    //to handle our submit form
+  _saveAndContinue(e) {
     e.preventDefault();
     // let filename  = this.video.value.replace("C:\\fakepath\\", "");
-    // console.log('This is filename:', filename);
     // let trueVideo  = secret.endpointLocation + '/' + secret.bucketName + '/' + filename 
     let uInfo = {
       firstName : this.firstName.value,
@@ -28,52 +21,15 @@ var CreateAccountScreen = React.createClass({
       companyName : this.companyName.value,
       phoneNumber : this.phoneNumber.value,
       loggedIn: true
-    // image : this.image.value
     }
-    // uInfo.video.push(trueVideo);
-    // console.log('this is video test for uInfo', uInfo);
-// ==================================================================
-    axios.post('/users/register', uInfo)
-    .then(function(response){
-      //userInfo is the response back with the very last user entered
-      let userInfo = response.data;
-      //sets "user" in localstorage to what is contained in userInfo
-      // console.log('this is userInfo before server:', uInfo);
-      // console.log('this is userInfo after server:', userInfo);
-      localStorage.setItem('user', JSON.stringify(userInfo))
-      // localStorage.setItem('newUser', userInfo)
-      // console.log('this newUser:', userInfo);
-    })
-    .then(function(){
-      //redirects to the profile page
-      hashHistory.push('profile')
-    })  
-/*======================================================================*/
-    //  // to handle our submit form
-    // //the variable form below is used to grab the entire form element
-    // var form = document.querySelector("form");
-    // console.log('this is form:', form);
-    // //the variable fdata will be the actual form that will have the new file uploaded
-    // var fdata = new FormData(form);
-    // console.log('this is fdata:', fdata);
-    // // send fdata to our server to upload file to s3
-    // axios.post('/users/video', fdata)
-    // .then(function(){
-    // console.log('File uploaded successfully')
-    // })  
-    // .then(function(){
-    //   //redirects to the profile page
-    //   hashHistory.push('profile')
-    // })  
-
-},
+    this.props.register(uInfo)
+  }
 
  
 
-  render: function() {
+  render () {
     return (
       <div className="create_account_screen">
-
         <div className="create_account_form">
           <h1>Create account</h1>
           <form onSubmit={this._saveAndContinue}>
@@ -137,14 +93,13 @@ var CreateAccountScreen = React.createClass({
               name="submit"
               value="Register"
               className="register-button" />
-        
           </form>
         </div>
-
       </div>
     );
   }
+};
     
-});
-    
-module.exports = CreateAccountScreen;
+export default connect(null, { register })(CreateAccountScreen);
+
+
