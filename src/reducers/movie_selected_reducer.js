87b43fd16097e7be1ff6bad0dd51df69713a0c0b
Yeiber Cano <yeibercano@gmail.com>
2016-08-31 23:58:33 -0700

    create movie_selected reducer

---
+++ src/reducers/movie_selected_reducer.js
@@ -0,0 +1,11 @@
+import { MOVIE_SELECTED } from './actions/types'
+
+export function movieSelected(state = {movieSelected:''}, action) {
+	switch (action.type) {
+		case MOVIE_SELECTED:
+			return {
+				...state, movieSelected: action.payload
+			}
+	}
+	return state
+}
