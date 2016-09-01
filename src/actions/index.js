97a1f39d0c7f16d3bda7e8d61b2aef4ac7cef452
Yeiber Cano <yeibercano@gmail.com>
2016-09-01 01:05:59 -0700

    refactor movieSelected action

--- src/actions/index.js
+++ src/actions/index.js
@@ -41,7 +41,7 @@ export const userLastMovie = (username) => {
 
 export const movieSelected = (movie) => {
   return dispatch => {
-    dispatch({ type:types.MOVIE_SELECTED, payload: movie})
+    dispatch({ type: types.MOVIE_SELECTED, payload: movie})
     localStorage.setItem('viewerMovie', JSON.stringify(movie));
     hashHistory.push('viewer')  
   }
