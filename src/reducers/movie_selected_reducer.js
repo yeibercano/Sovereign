4ddb5c09494d951bf2d1b81632a4631936c283b0
Yeiber Cano <yeibercano@gmail.com>
2016-09-01 01:13:47 -0700

    remove reference to console.log

--- src/reducers/movie_selected_reducer.js
+++ src/reducers/movie_selected_reducer.js
@@ -1,7 +1,6 @@
 import { MOVIE_SELECTED } from '../actions/types'
 
 export function movieSelected(state = {movieSelected:''}, action) {
-	console.log('action', action)
 	switch (action.type) {
 		case MOVIE_SELECTED:
 			return {
