1e1b672b4ac1623eb2634117e8179926638ff135
Yeiber Cano <yeibercano@gmail.com>
2016-09-07 23:30:55 -0700

    refactor functional stateless & ES6 destructuring

--- src/Components/ProfileViewComponents/profileInfo.js
+++ src/Components/ProfileViewComponents/profileInfo.js
@@ -1,26 +1,24 @@
 import React, {Component} from 'react'
 import Gravatar from 'react-gravatar'
  
-class ProfileInfo extends Component {
-
-  render() {
-    return (
-      <aside  className="account_information">
-        <h1 id="account_information_title" >Welcome, {this.props.userInfo.userName}!</h1>
-        <section id="account_information_gravatar_container">
+// class ProfileInfo extends Component {
+const ProfileInfo = ({ userInfo}) => {
+  return (
+    <aside  className="account_information">
+      <h1 id="account_information_title" >Welcome, {userInfo.userName}!</h1>
+      <section id="account_information_gravatar_container">
         <Gravatar 
           id="account_information_gravatar" 
-          email={this.props.userInfo.email.toLowerCase().trim()} 
+          email={userInfo.email.toLowerCase().trim()} 
           default="monsterid" size={200} rating="pg">
         </Gravatar>
-        </section>
-        <h4 id="full_name">{this.props.userInfo.firstName} {this.props.userInfo.lastName}</h4>
-        <h4>{this.props.userInfo.email}</h4>
-        <h4>{this.props.userInfo.companyName}</h4>
-        <h4>{this.props.userInfo.website}</h4>
-      </aside>
-    );
-  }
+      </section>
+      <h4 id="full_name">{userInfo.firstName} {userInfo.lastName}</h4>
+      <h4>{userInfo.email}</h4>
+      <h4>{userInfo.companyName}</h4>
+      <h4>{userInfo.website}</h4>
+    </aside>
+  );
 }
 
 export default ProfileInfo
