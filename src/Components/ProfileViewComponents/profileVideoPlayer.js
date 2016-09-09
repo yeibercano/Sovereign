a90a264f17d3b50db0987d3c6d93c8b339472ebe
Yeiber Cano <yeibercano@gmail.com>
2016-09-08 22:03:06 -0700

    refactor setState with Redux

--- src/Components/ProfileViewComponents/profileVideoPlayer.js
+++ src/Components/ProfileViewComponents/profileVideoPlayer.js
@@ -1,18 +1,20 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ProfileVideoList from './profileVideoList'
 import { connect } from 'react-redux'
-import { userMovies } from '../../actions' 
+import { userMovies, userMoviesSelected } from '../../actions' 
 
 class ProfileVideoPlayer extends Component {
+  
+
   componentWillMount() {
     const { userInfo } = this.props;
     this.props.userMovies(userInfo.userName);
   }
   selectedMovie (movie) {
-    this.setState({url: movie.video})
+    this.props.userMoviesSelected(movie)
   }
   render() {
-    const { allMovies, url, recentAdded } = this.props;
+    const { allMovies, url } = this.props;
     return (
       <section className="profile_video_player">
         <video controls src={url} type="video/mp4" />
@@ -26,11 +28,10 @@ class ProfileVideoPlayer extends Component {
 
 function mapStateToProps(state) {
   return { 
-    recentAdded: state.userMovies.recentMovie,
     url: state.userMovies.recentMovieUrl, 
     allMovies: state.userMovies.userMovies
   } 
 }
 
-export default connect(mapStateToProps, { userMovies })(ProfileVideoPlayer)
+export default connect(mapStateToProps, { userMovies, userMoviesSelected })(ProfileVideoPlayer)
 
