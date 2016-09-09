44c0e125a66e6385814d7f81699e8f5ef1be1757
Yeiber Cano <yeibercano@gmail.com>
2016-09-08 21:25:01 -0700

    remove last and import userMovies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import { category } from './receive_category_reducer'
 import { movies } from './receive_movies_reducer'
-import { last } from './user_last_movie_reducer'
+import { userMovies } from './user_movies_reducer'
 import { signedUser } from './signin_reducer'
 import { search } from './search_reducer'
 import { register } from './register_reducer'
@@ -11,7 +11,7 @@ import { reducer as form } from 'redux-form'
  const reducers = combineReducers({
  	category,
  	movies,
- 	last,
+ 	userMovies,
  	signedUser,
  	form, 
  	search,
