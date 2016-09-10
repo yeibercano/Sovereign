bcd616dfdc71a1378ba5482cbeccb34a4aff9f31
Yeiber Cano <yeibercano@gmail.com>
2016-09-10 01:29:34 -0700

    set movieSelected to default null

--- src/reducers/movie_selected_reducer.js
+++ src/reducers/movie_selected_reducer.js
@@ -1,6 +1,6 @@
 import { MOVIE_SELECTED } from '../actions/types'
 
-export function movieSelected(state = {viewerMovie:''}, action) {
+export function movieSelected(state = {viewerMovie: null}, action) {
 	switch (action.type) {
 		case MOVIE_SELECTED:
 			return {
