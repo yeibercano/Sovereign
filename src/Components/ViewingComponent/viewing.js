4917412813d89ed8e0634e9533939d52eb946ef6
Yeiber Cano <yeibercano@gmail.com>
2016-09-10 01:30:54 -0700

    refactor to Redux - remove localStorage/constructor

--- src/Components/ViewingComponent/viewing.js
+++ src/Components/ViewingComponent/viewing.js
@@ -3,53 +3,53 @@ import { connect } from 'react-redux'
 
 class ViewingPage extends Component {
 
-  constructor (props) {
-    super (props) 
+  // constructor (props) {
+  //   super (props) 
 
-    let movieSelected = localStorage.getItem('viewerMovie')
-    if (movieSelected === '{}') {
-      this.state = {
-        videoUrl: null
-      }
-    } else {
-      movieSelected = JSON.parse(movieSelected);
-      this.state = {
-        videoUrl: movieSelected.video,
-        videoImage: movieSelected.image,
-        videoTitle: movieSelected.title,
-        videoSynopsis: movieSelected.synopsis,
-        videoActors: movieSelected.actors,
-        videoDirector: movieSelected.director,
-        videoYear: movieSelected.year
-      }
-    }
-  }
+  //   let movieSelected = localStorage.getItem('viewerMovie')
+  //   if (movieSelected === '{}') {
+  //     this.state = {
+  //       videoUrl: null
+  //     }
+  //   } else {
+  //     movieSelected = JSON.parse(movieSelected);
+  //     this.state = {
+  //       videoUrl: movieSelected.video,
+  //       videoImage: movieSelected.image,
+  //       videoTitle: movieSelected.title,
+  //       videoSynopsis: movieSelected.synopsis,
+  //       videoActors: movieSelected.actors,
+  //       videoDirector: movieSelected.director,
+  //       videoYear: movieSelected.year
+  //     }
+  //   }
+  // }
  
   render() {
-
-    if (this.state.url === null) {
+    const { selectedMovie } = this.props;
+    if (selectedMovie === null) {
       return <div>Loading...</div>
     }
 
     return (
       <section>
-        <h1 id="viewer_title">{this.state.videoTitle}</h1>
+        <h1 id="viewer_title">{selectedMovie.title}</h1>
         <section >
-          <video className="viewing_video" autoPlay controls src={this.state.videoUrl} type="video/mp4" />
+          <video className="viewing_video" autoPlay controls src={selectedMovie.video} type="video/mp4" />
         </section>  
         <aside className="viewing_info">
             <section id="poster_info">
-              <img src={this.state.videoImage} />
+              <img src={selectedMovie.image} />
             </section>
             <section id="movie_info">
-            <h2>{this.state.videoTitle}</h2>
+            <h2>{selectedMovie.title}</h2>
               <section id="sub_movie_info">
                 <section id="synopsis">
-                <h4>{this.state.videoSynopsis}</h4>
+                <h4>{selectedMovie.synopsis}</h4>
                 </section>
-                <h4>Actors: {this.state.videoActors}</h4>
-                <h4>Director: {this.state.videoDirector}</h4>
-                <h4>Release: {this.state.videoYear}</h4>
+                <h4>Actors: {selectedMovie.actors}</h4>
+                <h4>Director: {selectedMovie.director}</h4>
+                <h4>Release: {selectedMovie.year}</h4>
               </section>
             </section>
         </aside>
@@ -58,6 +58,11 @@ class ViewingPage extends Component {
   }
 }
 
-export default connect(null, null)(ViewingPage)
+function mapStateToProps(state) {
+  return {
+    selectedMovie: state.movieSelected.viewerMovie
+  }
+}
+export default connect(mapStateToProps, null)(ViewingPage)
 
 
