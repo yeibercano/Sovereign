b428a6462720739319c60a109c1c086749a5d390
Yeiber Cano <yeibercano@gmail.com>
2016-09-11 20:55:17 -0700

    refactor new authUser

--- src/Components/ProfileViewComponents/profile.js
+++ src/Components/ProfileViewComponents/profile.js
@@ -27,7 +27,7 @@ class Profile extends Component {
 }
 
 function mapStateToProps(state) {
-   return { userInfo: state.signedUser.signedUser }
+   return { userInfo: state.authUser.authUser }
 }
 
 export default connect(mapStateToProps, null)(Profile)
