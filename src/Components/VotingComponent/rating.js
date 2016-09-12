ed21cedc2a67d38d08a570b722d410e63e614817
Yeiber Cano <yeibercano@gmail.com>
2016-09-11 22:36:15 -0700

    refactor w/Redux remove constructor and setState

--- src/Components/VotingComponent/rating.js
+++ src/Components/VotingComponent/rating.js
@@ -2,42 +2,56 @@ import React, { Component } from 'react';
 import ReactStars from 'react-stars';
 import { hashHistory } from 'react-router';
 import axios from 'axios';
+import { connect } from 'react-redux'
+import { voting } from '../../actions/index'
  
 class Rating extends Component {
-  constructor() {
-    super();
-    let movieSelected = localStorage.getItem('viewerMovie')
-    movieSelected = JSON.parse(movieSelected);
-    let user = localStorage.getItem('user')
-    user = JSON.parse(user);
+  // constructor() {
+  //   super();
+  //   let movieSelected = localStorage.getItem('viewerMovie')
+  //   movieSelected = JSON.parse(movieSelected);
+  //   let user = localStorage.getItem('user')
+  //   user = JSON.parse(user);
 
-    this.state = {
-      rating: 1, 
-      title: movieSelected.title, 
-      voter: user.userName
-    };
-    this.ratingChanged = this.ratingChanged.bind(this);
-  }
+  //   this.state = {
+  //     rating: 1, 
+  //     title: movieSelected.title, 
+  //     voter: user.userName
+  //   };
+  //   this.ratingChanged = this.ratingChanged.bind(this);
+  // }
   ratingChanged(vote) {
-    this.setState({rating: vote}, function(){   
-      axios.post('/movies/rating', { rating: this.state.rating, title: this.state.title, voter: this.state.voter });
-      hashHistory.push("profile");
-    });
+    const { user, movieSelected } = this.props;
+    this.props.voting({ 
+      rating:vote, 
+      title: movieSelected.title, 
+      voter: user.userName 
+    })
+    // this.setState({rating: vote}, function(){   
+    //   axios.post('/movies/rating', { rating: this.state.rating, title: this.state.title, voter: this.state.voter });
+    //   hashHistory.push("profile");
+    // });
   }
 
   render() {
-    const { rating } = this.state;
+
     return (                
       <section className="stars">
         <ReactStars
           count={5}
-          onChange={vote=>this.ratingChanged(vote)}
+          onChange={vote => this.ratingChanged(vote)}
           size={24} 
           color2={'#ffd700'} />
       </section>
     );
   }
 }
-export default Rating
+
+function mapStateToProps(state) {
+  return {
+    user: state.authUser.authUser
+  }
+}
+export default connect(mapStateToProps, { voting })(Rating)
 
 
