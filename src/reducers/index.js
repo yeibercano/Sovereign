b925dfee2e0e52589b5804cc7a04a53e45ff9ef1
Yeiber Cano <yeibercano@gmail.com>
2016-09-11 20:58:47 -0700

    import authUser and remove signedUser

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import { movies } from './receive_movies_reducer'
 import { userMovies } from './user_movies_reducer'
-import { signedUser } from './signin_reducer'
+import { authUser } from './auth_reducer'
 import { search } from './search_reducer'
 import { register } from './register_reducer'
 import { movieSelected } from './movie_selected_reducer'
@@ -10,7 +10,7 @@ import { reducer as form } from 'redux-form'
  const reducers = combineReducers({
  	movies,
  	userMovies,
- 	signedUser,
+ 	authUser,
  	form, 
  	search,
  	register, 
