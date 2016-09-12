de1286276542e25a13f8a6551e04521b95fcf4d0
Yeiber Cano <yeibercano@gmail.com>
2016-09-11 22:34:04 -0700

    refactor props reference

--- src/Components/VotingComponent/votingVideoList.js
+++ src/Components/VotingComponent/votingVideoList.js
@@ -6,16 +6,16 @@ class VotingVideoList extends Component {
     return (
       <section className="vote_contain">
         <section id="voting_image_viewer">
-          <img src={this.props.movie.videoImage} />
+          <img src={this.props.movie.image} />
         </section>
         <section id="voting_information_viewer" >
-          <h2>{this.props.movie.videoTitle}</h2>
+          <h2>{this.props.movie.title}</h2>
         <section id="synopsis">
-          <h5>{this.props.movie.videoSynopsis}</h5>
+          <h5>{this.props.movie.synopsis}</h5>
         </section>
-          <h5>Actors:   {this.props.movie.videoActors}</h5>
-          <h5>Director:   {this.props.movie.videoDirector}</h5>
-          <h5>Released:   {this.props.movie.videoYear}</h5>
+          <h5>Actors:   {this.props.movie.actors}</h5>
+          <h5>Director:   {this.props.movie.director}</h5>
+          <h5>Released:   {this.props.movie.year}</h5>
         </section>
       </section>
     );
