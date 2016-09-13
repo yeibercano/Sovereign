73f364ee40b06e036e25b169ad37bea1aa7bbb56
Yeiber Cano <yeibercano@gmail.com>
2016-09-12 23:02:47 -0700

    remove constructor and api calls

--- src/Components/VotingComponent/rating.js
+++ src/Components/VotingComponent/rating.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
 import ReactStars from 'react-stars';
-import { hashHistory } from 'react-router';
-import axios from 'axios';
 import { connect } from 'react-redux'
 import { voting } from '../../actions/index'
  
 class Rating extends Component {
-  // constructor() {
-  //   super();
-  //   let movieSelected = localStorage.getItem('viewerMovie')
-  //   movieSelected = JSON.parse(movieSelected);
-  //   let user = localStorage.getItem('user')
-  //   user = JSON.parse(user);
-
-  //   this.state = {
-  //     rating: 1, 
-  //     title: movieSelected.title, 
-  //     voter: user.userName
-  //   };
-  //   this.ratingChanged = this.ratingChanged.bind(this);
-  // }
   ratingChanged(vote) {
     const { user, movieSelected } = this.props;
     this.props.voting({ 
@@ -27,10 +11,6 @@ class Rating extends Component {
       title: movieSelected.title, 
       voter: user.userName 
     })
-    // this.setState({rating: vote}, function(){   
-    //   axios.post('/movies/rating', { rating: this.state.rating, title: this.state.title, voter: this.state.voter });
-    //   hashHistory.push("profile");
-    // });
   }
 
   render() {
@@ -47,11 +27,8 @@ class Rating extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    user: state.authUser.authUser
-  }
-}
+const mapStateToProps = (state) => ({ user: state.authUser.authUser })
+
 export default connect(mapStateToProps, { voting })(Rating)
 
 
