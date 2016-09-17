b412790d6fb2ac0c04e97e1fecccde202d1700bc
Yeiber Cano <yeibercano@gmail.com>
2016-09-16 21:31:19 -0700

    refactor to use Redux Form

--- src/Components/register.js
+++ src/Components/register.js
@@ -27,6 +27,7 @@ class CreateAccountScreen extends Component {
   // }
 
   handleFormSubmit(user){
+    console.log('user', user)
     this.props.register(user)
   }
 
@@ -46,67 +47,17 @@ class CreateAccountScreen extends Component {
       <div className="create_account_screen">
         <div className="create_account_form">
           <h1>Create account</h1>
-          <form onSubmit={this.handleSubmit(this.handleFormSubmit.bind(this))}>
-            <input
-              type="text"
-              ref={input=> this.firstName = input} 
-              require={true}
-              name="firstName"
-              placeholder ="Enter First Name"
-             />
-            <input
-              type="text" 
-              name="lastName"
-              placeholder ="Enter Last Name"
-              ref={input=> this.lastName = input}
-              />
-            <input
-              type="text"
-              name="userName" 
-              placeholder ="Enter desired username"
-              ref={input=> this.userName = input}
-              />
-            <input
-              type="password" 
-              name="password" 
-              placeholder ="Enter Password"
-              ref={input=> this.password = input}
-              />
-            <input
-              type="password" 
-              name="confirmPassword"
-              placeholder ="Re-enter Password"
-              ref={input=> this.confirmPassword = input} 
-              />
-            <input
-              type="text"
-              name="email" 
-              placeholder ="Enter E-mail"
-              ref={input=> this.email = input}
-              />
-            <input
-              type="text" 
-              name="website"
-              placeholder ="Enter Your Personal Website"
-              ref={input=> this.website = input} 
-              />
-            <input
-              type="text" 
-              name="companyName"
-              placeholder ="Enter The Name Of Your Company"
-              ref={input=> this.companyName = input}  
-              />
-            <input
-              type="text" 
-              name="phoneNumber"
-              placeholder ="Enter Your Phone Number"
-              ref={input=> this.phoneNumber = input} 
-              />
-            <input 
-              type="submit" 
-              name="submit"
-              value="Register"
-              className="register-button" />
+          <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <input type="text" {...firstName} placeholder="Enter First Name" require={true} />
+            <input type="text" {...lastName} placeholder ="Enter Last Name" />
+            <input type="text" {...userName} placeholder ="Enter desired username" />
+            <input type="password" {...password} placeholder ="Enter Password" />
+            <input type="password" {...confirmPassword} placeholder ="Re-enter Password" />
+            <input type="text" {...email} placeholder ="Enter E-mail" />
+            <input type="text" {...website} placeholder="Enter Your Personal Website" />
+            <input type="text" {...companyName} placeholder ="Enter The Name Of Your Company" />
+            <input type="text" {...phoneNumber} placeholder="Enter Your Phone Number" />
+            <input type="submit" value="Register" className="register-button" />
           </form>
         </div>
       </div>
