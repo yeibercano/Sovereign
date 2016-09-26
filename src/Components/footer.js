c578c2bbd8de4772bc4192c9f2234e62967b5023
Yeiber Cano <yeibercano@gmail.com>
2016-09-25 20:16:19 -0700

    add github reference code to project

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -5,7 +5,8 @@ const Footer = () => {
 
   return (
     <footer className="footerBackground"> 
-      <h3>Sovereign &copy; { date }</h3>
+      <h3 className='footerText'>Sovereign &copy; { date } &nbsp; | &nbsp;</h3>
+      <h3 className='footerText'><a href="https://github.com/yeibercano/Sovereign"> github project</a></h3>
     </footer>
   );
 }
