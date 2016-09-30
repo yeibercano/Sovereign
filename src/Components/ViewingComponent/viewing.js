c96f04c70e3dd0d8a89ab54aef1d738871678ccf
Yeiber Cano <yeibercano@gmail.com>
2016-09-30 01:47:06 -0700

    refactor layout to match design

--- src/Components/ViewingComponent/viewing.js
+++ src/Components/ViewingComponent/viewing.js
@@ -10,27 +10,29 @@ class ViewingPage extends Component {
     }
 
     return (
-      <section>
-        <h1 id="viewer_title">{selectedMovie.title}</h1>
-        <section >
-          <video className="viewing_video" autoPlay controls src={selectedMovie.video} type="video/mp4" />
-        </section>  
-        <aside className="viewing_info">
-            <section id="poster_info">
-              <img src={selectedMovie.image} />
-            </section>
-            <section id="movie_info">
-            <h2>{selectedMovie.title}</h2>
-              <section id="sub_movie_info">
-                <section id="synopsis">
-                <h4>{selectedMovie.synopsis}</h4>
-                </section>
-                <h4>Actors:{selectedMovie.actors}</h4>
-                <h4>Director:{selectedMovie.director}</h4>
-                <h4>Release:{selectedMovie.year}</h4>
+      <section className="flexContainerColumn">
+        <h1 className="containerHeader">{selectedMovie.title}</h1>
+        <section>
+          <section className="centerContainer">
+            <video className="viewing_video" autoPlay controls src={selectedMovie.video} type="video/mp4" />
+          </section>  
+          <aside className="viewing_info">
+              <section id="poster_info">
+                <img src={selectedMovie.image} />
               </section>
-            </section>
-        </aside>
+              <section id="movie_info">
+              <h2>{selectedMovie.title}</h2>
+                <section id="sub_movie_info">
+                  <section id="synopsis">
+                    <h4>{selectedMovie.synopsis}</h4>
+                  </section>
+                  <h4>Actors:{selectedMovie.actors}</h4>
+                  <h4>Director:{selectedMovie.director}</h4>
+                  <h4>Release:{selectedMovie.year}</h4>
+                </section>
+              </section>
+          </aside>
+        </section>
       </section>
     );
   }
