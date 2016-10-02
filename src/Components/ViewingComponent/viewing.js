8f96fb19caa65443a7ef2fe85283125cb6e37f96
Yeiber Cano <yeibercano@gmail.com>
2016-10-01 23:51:20 -0700

    refactor layout viewing component with flexbox

--- src/Components/ViewingComponent/viewing.js
+++ src/Components/ViewingComponent/viewing.js
@@ -10,27 +10,27 @@ class ViewingPage extends Component {
     }
 
     return (
-      <section className="flexContainerColumn">
+      <section id="viewingContainer" className="flexContainerColumn">
         <h1 className="containerHeader">{selectedMovie.title}</h1>
-        <section>
-          <section className="centerContainer">
+        <section id="movieContainer" className="centerContainer">
+          <section id="player">
             <video className="viewing_video" autoPlay controls src={selectedMovie.video} type="video/mp4" />
           </section>  
-          <aside className="viewing_info">
-              <section id="poster_info">
-                <img src={selectedMovie.image} />
-              </section>
-              <section id="movie_info">
+          <aside id="movieInfoContainer" className="viewing_info movieInfoContainer">
+            <section id="poster_info">
+              <img src={selectedMovie.image} />
+            </section>
+            <section id="movie_info">
               <h2>{selectedMovie.title}</h2>
-                <section id="sub_movie_info">
-                  <section id="synopsis">
-                    <h4>{selectedMovie.synopsis}</h4>
-                  </section>
-                  <h4>Actors:{selectedMovie.actors}</h4>
-                  <h4>Director:{selectedMovie.director}</h4>
-                  <h4>Release:{selectedMovie.year}</h4>
+              <section id="sub_movie_info">
+                <section id="synopsis">
+                  <h4>{selectedMovie.synopsis}</h4>
                 </section>
+                <h4>Actors:{selectedMovie.actors}</h4>
+                <h4>Director:{selectedMovie.director}</h4>
+                <h4>Release:{selectedMovie.year}</h4>
               </section>
+            </section>
           </aside>
         </section>
       </section>
