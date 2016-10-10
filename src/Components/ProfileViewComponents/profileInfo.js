64930b6069618e6394b9728a6e477eb99e041cba
Yeiber Cano <yeibercano@gmail.com>
2016-10-09 22:36:13 -0700

    remove extra imports and form

--- src/Components/ProfileViewComponents/profileInfo.js
+++ src/Components/ProfileViewComponents/profileInfo.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react'
 import Gravatar from 'react-gravatar'
 
-const ProfileInfo = ({ userInfo }) => {
-  return (
-    <aside  className="account_information">
-      <h1 id="account_information_title" >Welcome, {userInfo.userName}!</h1>
-      <section id="account_information_gravatar_container">
-        <Gravatar 
-          id="account_information_gravatar" 
-          email={userInfo.email.toLowerCase().trim()} 
-          default="monsterid" size={200} rating="pg">
-        </Gravatar>
-      </section>
-      <h4 id="full_name">{userInfo.firstName} {userInfo.lastName}</h4>
-      <h4>{userInfo.email}</h4>
-      <h4>{userInfo.companyName}</h4>
-      <h4>{userInfo.website}</h4>
-    </aside>
-  );
+class ProfileInfo extends Component {
+
+ render () {
+    const { userInfo } = this.props;
+    return (
+      <aside  contenteditable="true" className="account_information">
+        <h1 id="account_information_title" >Welcome, {userInfo.userName}!</h1>
+        <section id="account_information_gravatar_container">
+          <Gravatar 
+            id="account_information_gravatar" 
+            email={userInfo.email.toLowerCase().trim()} 
+            default="monsterid" size={200} rating="pg">
+          </Gravatar>
+        </section>
+        <h4 id="full_name">{userInfo.firstName} {userInfo.lastName}</h4>
+        <h4>{userInfo.email}</h4>
+        <h4>{userInfo.companyName}</h4>
+        <h4>{userInfo.website}</h4>
+      </aside>
+    );
+  }
 }
 
-export default ProfileInfo
+export default (ProfileInfo)
